a534b12446c554ad09719343ae3678e74238adcf
Виктор Коняев <vikongoo@gmail.com>
2022-10-14 08:57:30 +0300

    Юнит-тесты

---
+++ SenderService.Tests/AbstractTest.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NTB.SenderService.Data;
+
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)]
+namespace NTB.SenderService.Tests
+{
+    [TestClass]
+    public abstract class AbstractTest : IDisposable
+    {
+        protected ServiceProvider ServiceProvider { get; set; }
+
+        private IServiceCollection serviceCollection { get; set; }
+
+        [TestInitialize]
+        public void TestInitialization()
+        {
+            var host = Host
+                .CreateDefaultBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                })
+                //.UseStartup<Startup>()
+                .Build();
+
+            serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
+            //serviceCollection.RegisterOwnLogic(host.Services.GetService<IConfiguration>());
+            //serviceCollection.AddHttpContextAccessor();
+            serviceCollection.AddDbContext<DatabaseContext>(options =>
+            {
+                options
+                    .UseInMemoryDatabase("Messages")
+                    //Отключаем ошибки, что поведение транзакций в InMemory отличается от реляционного
+                    .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var dbContext = ServiceProvider.GetService<DatabaseContext>();
+            dbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            var dbContext = ServiceProvider.GetService<DatabaseContext>();
+            dbContext.Dispose();
+
+            serviceCollection = null;
+            ServiceProvider = null;
+        }
+
+    }
+}
---
+++ SenderService.Tests/ErrorsTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using NTB.SenderService.Data;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Diagnostics;
+
+namespace NTB.SenderService.Tests
+{
+	[TestClass]
+	public class ErrorsTests: AbstractTest
+	{
+		[TestMethod]
+		public async Task AddError_ToMessage_Successful()
+		{
+			DatabaseContext ctx = ServiceProvider.GetService<DatabaseContext>();
+			var messages = ctx.Messages;
+			var msg = new Message {
+				Recipient="TestSystem", 
+				Subject="Test Error", 
+				TypeId= MessageTypeEnum.Telegram 
+			};
+			messages.Add(msg);
+			await ctx.SaveChangesAsync();
+			var msgId = msg.Id;
+			var msgWithError = messages.FirstOrDefault(m => m.Id == msgId);
+			var err = msgWithError.SetError(MessageErrorTypeEnum.Program, "test error");
+			await ctx.SaveChangesAsync();
+			var savedErr = ctx.MessageErrors.FirstOrDefault(t => t.MessageId == msgId);
+
+			Assert.IsNotNull(savedErr);
+			Assert.AreEqual(err.Describe, savedErr.Describe);
+
+		}
+
+		[TestMethod]
+		public async Task ChangeError_InMessage_ChangesError()
+		{
+			DatabaseContext ctx = ServiceProvider.GetService<DatabaseContext>();
+			var messages = ctx.Messages;
+			var msg = new Message
+			{
+				Recipient = "TestSystem",
+				Subject = "Test Error",
+				TypeId = MessageTypeEnum.Telegram
+			};
+			messages.Add(msg);
+			await ctx.SaveChangesAsync();
+
+			var msgId = msg.Id;
+			var errorText = "this is test error";
+			msg.SetError(MessageErrorTypeEnum.Program, errorText);
+			await ctx.SaveChangesAsync();
+			var msgWithError = messages.FirstOrDefault(m => m.Id == msgId);
+			var savedErr = ctx.MessageErrors.FirstOrDefault(t => t.MessageId == msgId);
+
+			Assert.IsNotNull(savedErr);
+			Assert.AreEqual(errorText, savedErr.Describe);
+
+		}
+
+		[TestMethod]
+		public async Task LastError_Returns_Error()
+		{
+			DatabaseContext ctx = ServiceProvider.GetService<DatabaseContext>();
+			var messages = ctx.Messages;
+			var msg = new Message
+			{
+				Recipient = "TestSystem",
+				Subject = "Test Error",
+				TypeId = MessageTypeEnum.Telegram
+			};
+			var errorText = "this is test error";
+			msg.SetError( MessageErrorTypeEnum.Program, errorText);
+			messages.Add(msg);
+			await ctx.SaveChangesAsync();
+			var msgId = msg.Id;
+
+			var msgWithError = messages.FirstOrDefault(m => m.Id == msgId);
+			
+			Assert.AreEqual(errorText, msgWithError.LastError.Describe);
+
+		}
+	}
+}
